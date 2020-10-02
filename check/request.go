51b9601c89cbdff69c8139eab0e9ee27b9849988
Vitaly <Vitaly.Arbuzov@gmail.com>
2020-10-01 20:19:33 -0700

    Adding comments and extracting alerts into a separate field

--- check/request.go
+++ check/request.go
@@ -1,10 +1,12 @@
 package check
 
+// VersionCheckRequest provides basic info about the client and is used to produce VersionCheckResponse.
 type VersionCheckRequest struct {
-	Product string
-	Version string
-	Arch    string
-	OS      string
-	DB      string
-	Token   string
+	Product   string
+	Version   string
+	Arch      string
+	OS        string
+	DB        string
+	ClusterID string
+	Token     string
 }
--- check/response.go
+++ check/response.go
@@ -1,23 +1,35 @@
 package check
 
-const (
-	Unspecified Severity = iota
-	High
-	Medium
-	Low
-)
-
+// Severity defines importance of the upgrade from the current version to the recommended version.
 type Severity int
 
+const (
+	// SeverityUnspecified means that severity hasn't been set or is unknown.
+	SeverityUnspecified Severity = iota
+	// SeverityHigh means that there are important updates available for your version and they should be applied ASAP.
+	SeverityHigh
+	// SeverityMedium means that there are important updates available however neither of them is urgent.
+	SeverityMedium
+	// SeverityLow means that no major changes are available and update is not required.
+	SeverityLow
+)
+
+// ReleaseInfo contains information about a specific version of the product.
 type ReleaseInfo struct {
 	Version     string `json:"version"`
 	ReleaseDate int64  `json:"release_date"`
 	Notes       string `json:"notes"`
 }
 
+// Alert contains message and severity of a given update.
+type Alert struct {
+	Message  string   `json:"message"`
+	Severity Severity `json:"severity"`
+}
+
+// VersionCheckResponse contains recommendation about the best version of product and
 type VersionCheckResponse struct {
 	Current     ReleaseInfo `json:"current"`
 	Recommended ReleaseInfo `json:"recommended"`
-	Severity    Severity    `json:"important"`
-	Message     string      `json:"message"`
+	Alerts      []Alert     `json:"alerts"`
 }
