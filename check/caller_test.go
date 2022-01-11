bb9eba6090de61fcc73654ff8a5045a1fedb9c08
Roey Berman <roey@temporal.io>
2022-01-10 22:58:38 -0800

    Switch to POST JSON and add sdk info (#8)
    * Switch to POST JSON and add sdk info
    
    * Address CR comments
    
    * Use camel case for JSON properties

---
+++ check/caller_test.go
@@ -0,0 +1,78 @@
+package check
+
+import (
+	"encoding/json"
+	"testing"
+	"time"
+	"io/ioutil"
+	"net/url"
+	"net/http"
+	"net/http/httptest"
+)
+
+
+func TestPostInfo(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "POST" {
+			t.Errorf("Method != POST (%s)", r.Method)
+		}
+		if r.URL.Path != "/check" {
+			t.Errorf("URL.Path != /check (%s)", r.URL.Path)
+		}
+		if r.Header.Get("Content-Type") != "application/json" {
+			t.Errorf("Content-Type != application/json (%s)", r.Header.Get("Content-Type"))
+		}
+		defer r.Body.Close()
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			t.Fatalf("Failed to read request body %s", err)
+		}
+		versionCheckRequest := &VersionCheckRequest{}
+		err = json.Unmarshal(body, versionCheckRequest)
+		if err != nil {
+			t.Fatalf("Failed to unmarshal request body %s", err)
+		}
+		// Unmarshalling works
+		res, err := json.Marshal(VersionCheckResponse{
+			Current:      ReleaseInfo {
+				Version: "0.1",
+				ReleaseTime: time.Now().UnixNano(),
+				Notes: "",
+			},
+			Recommended:  ReleaseInfo {
+				Version: "0.1",
+				ReleaseTime: time.Now().UnixNano(),
+				Notes: "",
+			},
+			Instructions: "instructions",
+			Alerts:       []Alert{},
+		})
+		if err != nil {
+			t.Fatalf("Failed to marshal response %s", err)
+		}
+		w.Write(res)
+	}))
+	url, err := url.Parse(ts.URL)
+	if err != nil {
+		t.Fatalf("Request failed: %s", err)
+	}
+	caller := &callerImpl{url.Scheme, url.Host}
+	sdkInfo := []SDKInfo{{
+		Name: "sdk-java",
+		Version: "3.11",
+		TimesSeen: 23,
+	}}
+	_, err = caller.Call(&VersionCheckRequest{
+		Product:   "server",
+		Version:   "0.1",
+		ClusterID: "foo",
+		DB:        "cassandra",
+		OS:        "linux",
+		Arch:      "arm64",
+		Timestamp: time.Now().UnixNano(),
+		SDKInfo:   sdkInfo,
+	})
+	if err != nil {
+		t.Fatalf("Request failed: %s", err)
+	}
+}
--- check/request.go
+++ check/request.go
@@ -1,12 +1,21 @@
 package check
 
 // VersionCheckRequest provides basic info about the client and is used to produce VersionCheckResponse.
+type SDKInfo struct {
+	Name      string `json:"sdkName"`
+	Version   string `json:"sdkVersion"`
+	TimesSeen int64  `json:"timesSeen"`
+}
+
+// VersionCheckRequest provides basic info about the client and is used to produce VersionCheckResponse.
 type VersionCheckRequest struct {
-	Product   string
-	Version   string
-	Arch      string
-	OS        string
-	DB        string
-	ClusterID string
-	Timestamp int64
+	Product   string `json:"product"`
+	Version   string `json:"version"`
+	Arch      string `json:"arch"`
+	OS        string `json:"os"`
+	DB        string `json:"db"`
+	ClusterID string `json:"cluster"`
+	// Encode as string for JS compatibility
+	Timestamp int64     `json:"timestamp,string"`
+	SDKInfo   []SDKInfo `json:"sdkInfo"`
 }
