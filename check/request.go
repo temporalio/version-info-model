f8aaf6126d244612fad6d3ffd32a29a649562d43
Roey Berman <roey.berman@gmail.com>
2022-01-10 22:58:16 -0800

    Use camel case for JSON properties

--- check/request.go
+++ check/request.go
@@ -2,9 +2,9 @@ package check
 
 // VersionCheckRequest provides basic info about the client and is used to produce VersionCheckResponse.
 type SDKInfo struct {
-	Name      string `json:"sdk_name"`
-	Version   string `json:"sdk_version"`
-	TimesSeen int64  `json:"times_seen"`
+	Name      string `json:"sdkName"`
+	Version   string `json:"sdkVersion"`
+	TimesSeen int64  `json:"timesSeen"`
 }
 
 // VersionCheckRequest provides basic info about the client and is used to produce VersionCheckResponse.
@@ -17,5 +17,5 @@ type VersionCheckRequest struct {
 	ClusterID string `json:"cluster"`
 	// Encode as string for JS compatibility
 	Timestamp int64     `json:"timestamp,string"`
-	SDKInfo   []SDKInfo `json:"sdk_info"`
+	SDKInfo   []SDKInfo `json:"sdkInfo"`
 }
